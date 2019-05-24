23d43e1042b5a11e41ad09091c0a9627622093cf
unknown7 <unknown7@live.cn>
2019-05-25 01:53:26 +0800

    sharing constrained resources

--- src/main/java/sharingConstrainedResources/cooperation/Car.java
+++ src/main/java/sharingConstrainedResources/cooperation/Car.java
@@ -1,23 +1,50 @@
 package sharingConstrainedResources.cooperation;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Car {
     private boolean waxed = false;
-    public synchronized void waxed() {
-        this.waxed = true;
-        System.err.println("waxed");
-        notifyAll();
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+    public void waxed() {
+        lock.lock();
+        try {
+            this.waxed = true;
+            System.err.println("waxed");
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
     }
-    public synchronized void buffed() {
-        this.waxed = false;
-        System.err.println("buffed");
-        notifyAll();
+    public void buffed() {
+        lock.lock();
+        try {
+            this.waxed = false;
+            System.err.println("buffed");
+            condition.signalAll();
+        } finally {
+            lock.unlock();
+        }
     }
-    public synchronized void waitingForWax() throws InterruptedException {
-        while (!this.waxed)
-            wait();
+    public void waitingForWax() throws InterruptedException {
+        lock.lock();
+        try {
+            while (!this.waxed)
+                condition.await();
+        } finally {
+            lock.unlock();
+        }
+
     }
     public synchronized void waitingForBuff() throws InterruptedException {
-        while (this.waxed)
-            wait();
+        lock.lock();
+        try {
+            while (this.waxed)
+                condition.await();
+        } finally {
+            lock.unlock();
+        }
     }
 }
